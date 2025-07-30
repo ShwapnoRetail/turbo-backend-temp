4a93d64b40448294968df83b42c56202669dcbc6
tanvirUpol <tanvirrahmanupol@gmail.com>
2025-07-31 02:04:47 +0600

    refactor(page): comment out unused imports and user greeting logic fix(turbo): update build outputs to include .next directory

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,20 +1,14 @@
 // import Image, { type ImageProps } from "next/image";
 // import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { prisma } from "@repo/db";
+// import { prisma } from "@repo/db";
 
 
 export default async function Home() {
-  const user = await prisma.user.findFirst()
+  // const user = await prisma.user.findFirst()
   return (
     <div className={styles.page}>
-        {
-          user ? (
-            <h1>Welcome, {user.name}!</h1>
-          ) : (
-            <h1>Welcome to the App!</h1>
-          )
-        }
+       hello world
     </div>
   );
 }
